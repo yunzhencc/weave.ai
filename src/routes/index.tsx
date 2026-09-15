77a6ec3bfc6b0ce7f92df87a2e0e6a767fce4be6
wangxingkang <wang_xingkang@qq.com>
2026-09-15 23:39:16 +0800

    feat(ui): add motion and theme toggle components

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,5 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { Button } from '#/components/ui/button';
 
 export const Route = createFileRoute('/')({ component: App })
 
@@ -6,6 +7,8 @@ function App() {
   return (
     <main>
       Hello World
+
+      <Button>测试</Button>
     </main>
   )
 }
